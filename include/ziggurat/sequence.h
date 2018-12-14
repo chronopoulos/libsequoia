8221f1ace48e8e510c70ee8b0db586efc9b7749f
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2018-12-14 04:47:50 -0500

    sequence: add basic translation from trig -> tick

--- include/ziggurat/sequence.h
+++ include/ziggurat/sequence.h
@@ -2,7 +2,10 @@
 #define SEQUENCE_H
 
 #include <pthread.h>
+#include <jack/midiport.h>
 #include <jack/ringbuffer.h>
+
+typedef jack_midi_data_t midi_packet[3]; // equivalent to 3 unsigned chars
 
 struct zig_sequence_data {
 
@@ -10,8 +13,10 @@ struct zig_sequence_data {
     int tps;
     jack_nframes_t fpt;
 
+    struct zig_trigger_data *trigs;
+
     int nticks;
-    int *ticks;
+    midi_packet *ticks;
 
     int tick;
     jack_nframes_t frame;
@@ -19,8 +24,8 @@ struct zig_sequence_data {
 };
 
 void zig_sequence_init(struct zig_sequence_data*, int, int, int);
-void zig_sequence_set_step(struct zig_sequence_data*, int, int);
-void zig_sequence_set_raw_tick(struct zig_sequence_data*, int, int);
+void zig_sequence_set_trig(struct zig_sequence_data*, int, struct zig_trigger_data*);
+void zig_sequence_set_raw_tick(struct zig_sequence_data*, int, midi_packet*);
 void zig_sequence_process(struct zig_sequence_data*, jack_nframes_t, void*);
 
 #endif
--- src/sequence.c
+++ src/sequence.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <jack/midiport.h>
 
 #include "ziggurat.h"
 
@@ -18,12 +17,18 @@ void zig_sequence_init(struct zig_sequence_data *seq, int nsteps, int tps, int f
     seq->tps = tps;
     seq->fpt = fpt;
 
-    seq->nticks = seq->nsteps * seq->tps;
-    seq->ticks = (int*) malloc(seq->nticks * sizeof(int));
-    for(int i=0; i<seq->nticks; i++) {
-        seq->ticks[i] = 0;
+    seq->trigs = malloc(seq->nsteps * sizeof(struct zig_trigger_data));
+    for (int i=0; i<seq->nsteps; i++) {
+        zig_trigger_init(seq->trigs + i);
     }
-    
+
+    seq->nticks = seq->nsteps * seq->tps;
+
+    seq->ticks = malloc(seq->nticks * sizeof(midi_packet));
+    for(int i=0; i<seq->nticks; i++) {
+        seq->ticks[i][0] = 0;
+    }
+
     // paged timekeeping: when frame reaches fpt,
     //  it rolls over and increments tick
     seq->tick = 0;
@@ -31,24 +36,44 @@ void zig_sequence_init(struct zig_sequence_data *seq, int nsteps, int tps, int f
 
 }
 
-void zig_sequence_set_step(struct zig_sequence_data *seq, int stepIndex, int note) {
+void zig_sequence_set_trig(struct zig_sequence_data *seq, int stepIndex, struct zig_trigger_data *trig) {
 
-    seq->ticks[stepIndex*seq->tps] = note;
+    memcpy(seq->trigs + stepIndex, trig, sizeof(struct zig_trigger_data));
+
+    int tickIndex_on, tickIndex_off;
+    midi_packet pkt_on, pkt_off;
+    if (trig->type == TRIG_NOTE) {
+
+        pkt_on[0] = 143 + trig->channel; // note on
+        pkt_on[1] = trig->note;
+        pkt_on[2] = trig->velocity;
+        tickIndex_on = (stepIndex + trig->microtime)*seq->tps;
+        if (tickIndex_on < 0) tickIndex_on = 0;
+        if (tickIndex_on >= seq->nticks) tickIndex_on = seq->nticks - 1;
+        zig_sequence_set_raw_tick(seq, tickIndex_on, &pkt_on);
+
+        pkt_off[0] = 127 + trig->channel; // note off
+        pkt_off[1] = trig->note;
+        pkt_off[2] = trig->velocity; // what should we put here?
+        tickIndex_off = tickIndex_on + trig->length*seq->tps;
+        if (tickIndex_off < 0) tickIndex_off = 0;
+        if (tickIndex_off >= seq->nticks) tickIndex_off = seq->nticks - 1;
+        zig_sequence_set_raw_tick(seq, tickIndex_off, &pkt_off);
+
+    }
 
 }
 
-void zig_sequence_set_raw_tick(struct zig_sequence_data *seq, int tickIndex, int note) {
+void zig_sequence_set_raw_tick(struct zig_sequence_data *seq, int tickIndex, midi_packet *pkt) {
 
-    seq->ticks[tickIndex] = note;
+    memcpy(seq->ticks + tickIndex, pkt, sizeof(midi_packet));
 
 }
 
 void zig_sequence_process(struct zig_sequence_data *seq, jack_nframes_t nframes,
                             void *port_buf) {
 
-    char midi_msg[3];
     unsigned char *midi_msg_write_ptr;
-    int note;
     jack_nframes_t nframes_left, frame_inc;
 
     nframes_left = nframes;
@@ -58,14 +83,10 @@ void zig_sequence_process(struct zig_sequence_data *seq, jack_nframes_t nframes,
         // if we're on a tick boundary, output the event
         if (seq->frame == 0) {
 
-            if ((note = seq->ticks[seq->tick])) { // if note != 0
-
-                midi_msg[0] = 144; // chan 1 note on
-                midi_msg[1] = note;
-                midi_msg[2] = 100; // velocity
+            if (seq->ticks[seq->tick][0]) { // if status byte != 0
 
                 midi_msg_write_ptr = jack_midi_event_reserve(port_buf, nframes - nframes_left, 3);
-                memcpy(midi_msg_write_ptr, midi_msg, 3);
+                memcpy(midi_msg_write_ptr, seq->ticks[seq->tick], 3);
 
             }
 
